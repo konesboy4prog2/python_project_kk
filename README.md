33462815a6da9c5ddc774ce225f396c41b0ad90a
kkart <100359950+konesboy4prog2@users.noreply.github.com>
2022-03-03 14:54:09 +0100

    First commit of README & main

---
+++ README.md
@@ -0,0 +1,11 @@
+#Titel
+##Ausgangslage
+Beschreibung der Ausgangslage
+
+##Funktion und Projektidee
+
+##Workflow
+* Dateineingabe
+* Datenverarbeitung
+* Datenausgabe
+* Flussdiagramm zu WebApp
---
+++ main.py
@@ -0,0 +1,13 @@
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name == "__main__":
+    app.run(debug=True, Port=5000)
